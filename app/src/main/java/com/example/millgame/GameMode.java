b39de5b4bd4dfea184694a80bdc49c9be4e1740d
glozanoa <glozanoa@uni.pe>
2022-05-02 16:41:11 -0500

    move GameVariant and GameMode enumerato to inside MillGame class

--- app/src/main/java/com/example/millgame/GameMode.java
+++
@@ -1,6 +0,0 @@
-package com.example.millgame;
-
-public enum GameMode {
-    HUMAN_HUMAN,
-    HUMAN_ROBOT
-}
--- app/src/main/java/com/example/millgame/GameVariant.java
+++
@@ -1,8 +0,0 @@
-package com.example.millgame;
-
-public enum GameVariant {
-    THREE_MEN_MORRIS,
-    SIX_MEN_MORRIS,
-    NINE_MEN_MORRIS,
-    TWELVE_MEN_MORRIS
-}
