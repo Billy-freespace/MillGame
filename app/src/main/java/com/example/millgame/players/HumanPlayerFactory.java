d116175d76f58ede5e35f140356da83a30d72ea9
glozanoa <glozanoa@uni.pe>
2022-05-03 11:39:59 -0500

    initializing HumanPlayer and RobotPlayer factories

--- app/src/main/java/com/example/millgame/players/HumanPlayerFactory.java
+++ app/src/main/java/com/example/millgame/players/HumanPlayerFactory.java
@@ -1,4 +1,10 @@
 package com.example.millgame.players;
 
+import com.example.millgame.Board;
+import com.example.millgame.Piece;
+import com.example.millgame.Player;
+
 public class HumanPlayerFactory implements PlayerFactory{
+    public Player create(Piece.PieceColor color, Board board){return null;}
+    public Player createByLevel(Piece.PieceColor color, Board board, Player.PlayerLevel level){return null;}
 }
