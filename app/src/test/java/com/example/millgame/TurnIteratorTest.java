69a6c2e3c65487a15d0e73cd956e1c894fa5116c
glozanoa <glozanoa@uni.pe>
2022-05-04 01:28:04 -0500

    add unittest for TurnIterator class

--- app/src/test/java/com/example/millgame/TurnIteratorTest.java
+++ app/src/test/java/com/example/millgame/TurnIteratorTest.java
@@ -1,27 +1,28 @@
 package com.example.millgame;
 
 import com.example.millgame.MillGame.GameVariant;
+import com.example.millgame.boards.NineMMBoard;
 import com.example.millgame.pieces.PieceColor;
 import com.example.millgame.boards.BoardCreatorDirector;
 import com.example.millgame.players.HumanPlayer;
 
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 public class TurnIteratorTest {
-
-    /*
     private Player player0;
     private Player player1;
     private ArrayList<Player> players;
+    private NineMMBoard board; // default board
 
-    @Before
+    @BeforeAll
     public void createPlayers(){
-        Board board = BoardCreatorDirector.makeMMBoard(GameVariant.NINE_MEN_MORRIS);
+        board = (NineMMBoard) BoardCreatorDirector.makeMMBoard(GameVariant.NINE_MEN_MORRIS);
         player0 = new HumanPlayer(PieceColor.WHITE, board);
         player1 = new HumanPlayer(PieceColor.BLACK, board);
 
@@ -31,7 +32,7 @@ public class TurnIteratorTest {
     }
 
     @Test
-    public void testNext()
+    public void nextTurnTest()
     {
         TurnIterator itr = new TurnIterator(players);
 
@@ -41,7 +42,7 @@ public class TurnIteratorTest {
     }
 
     @Test
-    public void testGetOpponent() throws CloneNotSupportedException {
+    public void playerOpponentTest() throws CloneNotSupportedException {
         TurnIterator itr = new TurnIterator(players);
 
         Player player = itr.next(); // player0
@@ -50,5 +51,4 @@ public class TurnIteratorTest {
         assertEquals(player0, itr.getIterationState());
         assertEquals(player1, opponent);
     }
-     */
 }
