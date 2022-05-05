26bb4885dcc14abbb8230cec57ef6c782c5eb924
glozanoa <glozanoa@uni.pe>
2022-05-05 13:51:50 -0500

    update App class

--- app/src/main/java/com/example/millgame/App.java
+++ app/src/main/java/com/example/millgame/App.java
@@ -29,3 +29,22 @@ public class App implements Runnable {
         }
     }
 }
+
+/*
+
+public class App {
+    public static void main(String[] args){
+        Runnable runner = new Runnable(){
+            public void run(){
+                System.out.println("HELLO WORLD!");
+                // COMMAND LINE PARSER - TO ENABLE LOG TO CONSOLE
+
+                GameGUI gameGUI = new GameGUI();
+                gameGUI.setVisible(true);
+            }
+        };
+
+        EventQueue.invokeLater(runner);
+    }
+}
+ */
