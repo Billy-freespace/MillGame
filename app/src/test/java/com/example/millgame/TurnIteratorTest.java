fa6a9dc44eab76baf50ee3a7df2589edd046f988
glozanoa <glozanoa@uni.pe>
2022-05-08 11:40:54 -0500

    remove useless imports

--- app/src/test/java/com/example/millgame/TurnIteratorTest.java
+++ app/src/test/java/com/example/millgame/TurnIteratorTest.java
@@ -1,9 +1,7 @@
 package com.example.millgame;
 
 import com.example.millgame.MillGame.GameVariant;
-import com.example.millgame.boards.NineMMBoard;
 import com.example.millgame.pieces.PieceColor;
-import com.example.millgame.boards.BoardCreatorDirector;
 import com.example.millgame.players.HumanPlayer;
 
 
