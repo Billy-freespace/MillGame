4cd1a19b5d289735acaa56832e9bca0702b55dac
hpord31 <hannu.portillo.d@uni.pe>
2022-05-09 20:07:04 -0500

    Primera implementación de la prueba para verificar el número de fichas en el tablero del jugador

--- app/src/test/java/com/example/millgame/HumanPlayerTest.java
+++ app/src/test/java/com/example/millgame/HumanPlayerTest.java
@@ -3,7 +3,23 @@ package com.example.millgame;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.millgame.exceptions.NoPiecesError;
+import com.example.millgame.pieces.PieceColor;
+import com.example.millgame.players.HumanPlayer;
+
 public class HumanPlayerTest {
+
+    private MillGame  game = new MillGame();
+    private HumanPlayer player1 = new HumanPlayer(PieceColor.WHITE, game);
+ 
+    @Test
+    public void InvalidNoPieces () {
+        player1.npieces = 9;
+        assertThrows(NoPiecesError.class, () -> {
+            player1.placePiece('d', 1);;
+        });
+    }
+    
     @Test
      public void placePieceTest() {
          ;
--- app/src/test/java/com/example/millgame/PlayerTest.java
+++ app/src/test/java/com/example/millgame/PlayerTest.java
@@ -1,14 +1,28 @@
 package com.example.millgame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.example.millgame.boards.BoardCreatorDirector;
+import com.example.millgame.boards.NineMMBoard;
 import com.example.millgame.exceptions.InvalidPositionCoordinate;
 import com.example.millgame.exceptions.NoPiecesError;
+import com.example.millgame.pieces.PieceColor;
+import com.example.millgame.players.HumanPlayer;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 public  class PlayerTest {
 
-    @Test
-    public void placePieceTest () {
-        ;
+    private MillGame  game = new MillGame();
+    private HumanPlayer player1 = new HumanPlayer(PieceColor.WHITE, game);
+ 
+    
+
+    public void InvalidNoPieces () {
+        player1.npieces = 9;
+        assertThrows(NoPiecesError.class, () -> {
+            player1.placePiece('d', 1);;
+        });
     }
 }
