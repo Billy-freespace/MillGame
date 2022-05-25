0240163744d45e4f43f7cc64fc5de37a67ba4fd2
Billy-freespace <billy.freespace@gmail.com>
2022-05-25 10:10:30 -0500

    Coment removed

--- src/test/java/com/example/millgame/NineMMBoardBuildTest.java
+++ src/test/java/com/example/millgame/NineMMBoardBuildTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//@Disabled("PlayerFactory.create(PlayerType, Color, Board) method was delete - UPDATE")
 public class NineMMBoardBuildTest {
 
     private MillGame game;
