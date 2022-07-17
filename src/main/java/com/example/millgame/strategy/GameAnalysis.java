87c9a7e3af2a22ddbdc22ebe1019d6913c324fe1
hpord31 <hannu.portillo.d@uni.pe>
2022-07-17 12:06:35 -0500

    modificacion de clase Game Analysis

--- src/main/java/com/example/millgame/strategy/GameAnalysis.java
+++ src/main/java/com/example/millgame/strategy/GameAnalysis.java
@@ -9,12 +9,9 @@ import com.example.millgame.PieceRadar;
 import com.example.millgame.Position;
 
 public class GameAnalysis {
-<<<<<<< HEAD
     private MillGame game;
     private HashMap<Piece, List <Position>> opponentPosibleMills;
-=======
-    private HashMap<Piece, List<Position>> opponentPosibleMills;
->>>>>>> 48717b31c8c75faae684da7b479189e695978ace
+
     private HashMap<Piece, List <Position>> ownPosibleMills;
     private List<GameStep> playbooks;
     private int MAX_NUM_STEPS;
