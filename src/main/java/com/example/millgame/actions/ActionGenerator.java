4b4c1119e8a658126d6406e4ecc5db26c1c20036
Billy-freespace <billy.freespace@gmail.com>
2022-07-19 10:22:45 -0500

    Fixed ActionGenerator

--- src/main/java/com/example/millgame/actions/ActionGenerator.java
+++ src/main/java/com/example/millgame/actions/ActionGenerator.java
@@ -2,38 +2,42 @@
 package com.example.millgame.actions;
 
 import com.example.millgame.MillGame;
+import com.example.millgame.Piece;
 import com.example.millgame.Position;
+import com.example.millgame.exceptions.InvalidPositionCoordinate;
 import com.example.millgame.misc.BoardCoordinate;
+
+import java.awt.event.ActionEvent;
 
 /**
  * ActionGenerator
  */
 public class ActionGenerator {
 
-    private Millgame game;
+    private MillGame game;
     public ActionGenerator (MillGame game) {
         this.game = game;
     }
-    public  ActionEvent placePiece(BoardCoordinate coordinate) {
+    public ActionEvent placePiece(BoardCoordinate coordinate) throws InvalidPositionCoordinate {
         char x = coordinate.getX();
         int y = coordinate.getY();
-        Position position = game.getPosition(x, y);
+        Position position = game.getBoard().getPosition(x, y);
         ActionEvent event = new ActionEvent(position, -1, "RobotPlayer.autoPlacePiece ["+position + "]");
         return event;
     }
 
-    public  ActionEvent movePiece(Piece piece, BoardCoordinate coordinate) {
+    public  ActionEvent movePiece(Piece piece, BoardCoordinate coordinate) throws InvalidPositionCoordinate {
         char x = coordinate.getX();
         int y = coordinate.getY();
-        Position position = game.getPosition(x, y);
+        Position position = game.getBoard().getPosition(x, y);
         ActionEvent event = new ActionEvent(position, -1, "RobotPlayer.autoPlacePiece ["+position + "]");
         return event;
     }
 
-    public  ActionEvent removePiece(BoardCoordinate coordinate) {
+    public  ActionEvent removePiece(BoardCoordinate coordinate) throws InvalidPositionCoordinate {
         char x = coordinate.getX();
         int y = coordinate.getY();
-        Position position = game.getPosition(x, y);
+        Position position = game.getBoard().getPosition(x, y);
         ActionEvent event = new ActionEvent(position, -1, "RobotPlayer.autoPlacePiece ["+position + "]");
         return event;
     }
